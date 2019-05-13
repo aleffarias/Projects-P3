06d80c256f136796598aed16619df1d062956c71
aleffarias <aaof@ic.ufal.br>
2019-05-13 18:12:58 -0300

    Atualizando atributos

--- FolhaDePagamento/src/Empregado.java
+++ FolhaDePagamento/src/Empregado.java
@@ -1,10 +1,17 @@
 
 public class Empregado {
-	public int codigo;
+	public int numeroFuncionario;
 	public String nome;
 	public String endereco;
 	public int tipo;
 	public int metodoPagamento;
 	public int sindicato;
+	public int numeroSindicato;
+	
 	public double salario;
+	public float salarioHorista;
+	public float salarioFixo;
+	public float salarioComissionado;
+	public float percentualComissao;
+	
 }
