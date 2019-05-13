4d86cef57c500f43b5fdc698db35ddb5ef7aa785
aleffarias <aaof@ic.ufal.br>
2019-05-13 17:10:48 -0300

    Resolvendo problema de leitura da string

--- FolhaDePagamento/src/Main.java
+++ FolhaDePagamento/src/Main.java
@@ -13,49 +13,53 @@ public class Main {
 		while (key != 0) {
 			
 			System.out.println("Escolha uma opção:");
-			System.out.println("(1) Adicionar Empregado");
-			System.out.println("(2) Remover Empregado");
-			System.out.println("(3) Lançar um Cartão de Ponto");
-			System.out.println("(4) Lançar um Resultado Venda");
-			System.out.println("(5) Lançar uma taxa de serviço");
-			System.out.println("(6) Alterar detalhes de um empregado");
-			System.out.println("(7) Rodar a folha de pagamento para hoje");
-			System.out.println("(8) Undo/redo");
-			System.out.println("(9) Agenda de Pagamento");
-			System.out.println("(10) Criação de Novas Agendas de Pagamento");
-			System.out.println("(0) Sair");
+			System.out.println("(1) - Adicionar Empregado");
+			System.out.println("(2) - Remover Empregado");
+			System.out.println("(3) - Lançar um Cartão de Ponto");
+			System.out.println("(4) - Lançar um Resultado Venda");
+			System.out.println("(5) - Lançar uma taxa de serviço");
+			System.out.println("(6) - Alterar detalhes de um empregado");
+			System.out.println("(7) - Rodar a folha de pagamento para hoje");
+			System.out.println("(8) - Undo/redo");
+			System.out.println("(9) - Agenda de Pagamento");
+			System.out.println("(10) - Criação de Novas Agendas de Pagamento");
+			System.out.println("(0) - Sair");
 			
 			key = input.nextInt();	
+			input.nextLine();
+			
 			
 			switch (key) {
 			case 1:
 				Empregado perfil; 
 				perfil = new Empregado();
-						
+				
+				perfil.codigo = contador;
+				
 				System.out.println("Digite o nome do empregado:");
 				perfil.nome = input.nextLine();
 		
 				System.out.println("Digite o endereço do empregado:");
 				perfil.endereco = input.nextLine();
 				
-				System.out.println("\n(1) Horista\n(2) Assalariado\n(3) Comissionado");
+				System.out.println("\n(1) Horista\n(2) - Assalariado\n(3) - Comissionado");
 				perfil.tipo = input.nextInt();
 				
 				// Add atributos associados 
 				
-				System.out.println("\nMétodo de pagamento:\n(1) Cheque pelos correios\n(2) Cheque em mãos\n(3) Depósito em conta bancária");
+				System.out.println("\nMétodo de pagamento:\n(1) - Cheque pelos correios\n(2) - Cheque em mãos\n(3) - Depósito em conta bancária");
 				perfil.metodoPagamento = input.nextInt();
 				
-				System.out.println("\nPertence ao sindicato?\n(1) Sim\n(2) Não");
+				System.out.println("\nPertence ao sindicato?\n(1) - Sim\n(2) - Não");
 				perfil.sindicato = input.nextInt();
 				
-				perfil.codigo = contador;
 				
 				listaEmpregados.add(perfil);
 				contador++;
 				break;
 				}
 			}
+			System.out.println(listaEmpregados.get(0).nome);
 			input.close();
 	}
 }
