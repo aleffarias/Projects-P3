19a76ae05bb0662004a6b37e07904a3f92e78c16
aleffarias <aaof@ic.ufal.br>
2019-05-13 23:27:55 -0300

    Organizando main

--- FolhaDePagamento/src/Main.java
+++ FolhaDePagamento/src/Main.java
@@ -1,17 +1,14 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		int numeroFuncionario = 00, numeroSindicato = 00, key = -1;
+		int key = -1;
 		Scanner input = new Scanner(System.in);
-		
-		ArrayList<Empregado> listaEmpregados = new ArrayList<Empregado>();
-		
+				
 		while (key != 0) {
-			
+						
 			System.out.println("Escolha uma opção:");
 			System.out.println("(1) - Adicionar Empregado");
 			System.out.println("(2) - Remover Empregado");
@@ -26,81 +23,19 @@ public class Main {
 			System.out.println("(0) - Sair");
 			
 			key = input.nextInt();	
-			input.nextLine();
-			
+			//input.nextLine();
 			
 			switch (key) {
 			case 1:
-				Empregado perfil; 
-				perfil = new Empregado();
-				
-				perfil.numeroFuncionario = numeroFuncionario;
-				
-				System.out.println("Digite o nome do empregado:");
-				perfil.nome = input.nextLine();
-		
-				System.out.println("Digite o endereço do empregado:");
-				perfil.endereco = input.nextLine();
-				
-				System.out.println("\n(1) - Horista\n(2) - Assalariado\n(3) - Comissionado");
-				perfil.tipo = input.nextInt();
-				
-					switch (perfil.tipo) {
-					case 1:
-						System.out.print("Informe o salário por hora trabalhada:\nR$ ");
-						perfil.salarioHorista = input.nextFloat();						
-						break;
-						
-					case 2:
-						System.out.print("Informe o salário fixo mensal:\nR$ ");
-						perfil.salarioFixo = input.nextFloat();						
-						break;
-						
-					case 3:
-						System.out.print("Informe o salário:\nR$ ");
-						perfil.salarioComissionado = input.nextFloat();
-						System.out.println("Informe o percentual de comissão:");
-						perfil.percentualComissao = input.nextFloat();
-						
-						break;
-					
-					default:
-						System.out.println("Opção Inválida.");
-						
-					} 
-				
-				System.out.println("\nMétodo de pagamento:\n(1) - Cheque pelos correios\n(2) - Cheque em mãos\n(3) - Depósito em conta bancária");
-				perfil.metodoPagamento = input.nextInt();
-				
-				System.out.println("\nPertence ao sindicato?\n(1) - Sim\n(2) - Não");
-				perfil.sindicato = input.nextInt();
-				
-					if (perfil.sindicato == 1) {
-						perfil.numeroSindicato = numeroSindicato;
-						numeroSindicato++;
-					} else {
-						perfil.numeroSindicato = -1;
-					}
-				
-				
-				listaEmpregados.add(perfil);
-				System.out.println("Funcionário cadastrado com sucesso!\n");
-				numeroFuncionario++;
+				FuncoesEmpregado.adicionar();
 				break;
 			
 			case 2:
-				// Imprimir todos os funcionários
-				
-				System.out.println("Digite o número do Funcionário:");
-				int aux = input.nextInt();
-				listaEmpregados.remove(aux);
-				numeroFuncionario--;
+				FuncoesEmpregado.remover();
 				break;
 				}
 			}
 		
-			System.out.println(listaEmpregados.get(0).nome);
 			input.close();
 	}
 }
-
