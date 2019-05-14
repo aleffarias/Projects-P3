653674b8d8fc488cf75e0c57696c8cd6bc39cb69
aleffarias <aaof@ic.ufal.br>
2019-05-13 23:57:53 -0300

    Add opções do cartão de ponto

--- FolhaDePagamento/src/Empregado.java
+++ FolhaDePagamento/src/Empregado.java
@@ -1,17 +1,87 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class Empregado {
-	public int numeroFuncionario;
-	public String nome;
-	public String endereco;
-	public int tipo;
-	public int metodoPagamento;
-	public int sindicato;
-	public int numeroSindicato;
+public class Empregado { 
+		
+	public static Scanner input;
+	static ArrayList<RegistroEmpregado> listaEmpregados = new ArrayList<RegistroEmpregado>();
 	
-	public double salario;
-	public float salarioHorista;
-	public float salarioFixo;
-	public float salarioComissionado;
-	public float percentualComissao;
+	static int numeroFuncionario = 00, numeroSindicato = 00;
+
+	public static void adicionar() {
+
+		input = new Scanner(System.in);
+		
+		RegistroEmpregado perfil = new RegistroEmpregado();
+		
+		
+		perfil.numeroFuncionario = numeroFuncionario;
+		
+		System.out.println("Digite o nome do empregado:");
+		perfil.nome = input.nextLine();
+	
+		System.out.println("Digite o endereço do empregado:");
+		perfil.endereco = input.nextLine();
+		
+		System.out.println("\n(1) - Horista\n(2) - Assalariado\n(3) - Comissionado");
+		perfil.tipo = input.nextInt();
+		
+			switch (perfil.tipo) {
+			case 1:
+				System.out.print("Informe o salário por hora trabalhada:\nR$ ");
+				perfil.salarioHorista = input.nextFloat();						
+				break;
+				
+			case 2:
+				System.out.print("Informe o salário fixo mensal:\nR$ ");
+				perfil.salarioFixo = input.nextFloat();						
+				break;
+				
+			case 3:
+				System.out.print("Informe o salário:\nR$ ");
+				perfil.salarioComissionado = input.nextFloat();
+				System.out.println("Informe o percentual de comissão:\nExemplo: 1,5 - 0,6");
+				perfil.percentualComissao = input.nextFloat();
+				
+				break;
+			
+			default:
+				System.out.println("Opção Inválida.");
+				
+			} 
+		
+		System.out.println("\nMétodo de pagamento:\n(1) - Cheque pelos correios\n(2) - Cheque em mãos\n(3) - Depósito em conta bancária");
+		perfil.metodoPagamento = input.nextInt();
+		
+		System.out.println("\nPertence ao sindicato?\n(1) - Sim\n(2) - Não");
+		perfil.sindicato = input.nextInt();
+		
+			if (perfil.sindicato == 1) {
+				perfil.numeroSindicato = numeroSindicato;
+				numeroSindicato++;
+			} else {
+				perfil.numeroSindicato = -1;
+			}
+		
+		
+		listaEmpregados.add(perfil);
+		System.out.println("Funcionário cadastrado com sucesso!\n");
+		numeroFuncionario++;
+		
+	}
+	
+	public static void remover() {
+		// Imprimir todos os funcionários
+		
+		System.out.println("Digite o número do Funcionário:");
+		int aux = input.nextInt();
+		listaEmpregados.remove(aux);
+		numeroFuncionario--;
+		
+		// Teste
+		System.out.println(listaEmpregados.get(0).nome);
+		System.out.println(listaEmpregados.get(1).nome);
+		
+	}
 	
 }
---
+++ FolhaDePagamento/src/RegistroEmpregado.java
@@ -0,0 +1,26 @@
+
+public class RegistroEmpregado {
+	// Dados Pessoais
+	public int numeroFuncionario;
+	public String nome;
+	public String endereco;
+	public int tipo;
+	public int metodoPagamento;
+	public int sindicato;
+	public int numeroSindicato;
+	
+	// Salário
+	public double salario;
+	public float salarioHorista;
+	public float salarioFixo;
+	public float salarioComissionado;
+	public float percentualComissao;
+	
+	// Cartão de ponto
+	public int horaEntrada;
+	public int horaSaida;
+	public int dia;
+	public int mes;
+	public int ano;
+	
+}
